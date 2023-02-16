320634cfc15ec26c66c740e7fd39d622406dcc0d
vivianne <viviannjoroge91@gmail.com>
2023-02-16 17:05:03 +0300

    commit tree 1df5baa9d79e8201d81cfdc8e51219cd8a56545e

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO  bears (name,age,sex,color,temperament,alive) 
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'friendly', true),
+  ('Rowdy', 8, 'M', 'black', 'grumpy', true),
+  ('Tabitha', 4, 'F', 'gray', 'shy', true),
+  ('Sergeant Brown', 6, 'M', 'brown', 'disciplined', true),
+  ('Melissa', 2, 'F', 'white', 'playful', true),
+  ('Grinch', 10, 'M', 'green', 'scheming', true),
+  ('Wendy', 3, 'F', 'blonde', 'curious', true),
+  (NULL, 0, 'M', 'brown', 'grumpy', 1)
